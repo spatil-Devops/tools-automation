e0b42380f7cb7b45f6a6726182fb4fa7f860748a
Patil <shubham.patil5@dell.com>
2025-06-12 21:42:04 +0530

    prometheus

--- variables.tf
+++ variables.tf
@@ -1,9 +1,14 @@
 variable "tools" {
   default = {
-     vault = {
+    vault = {
        name          = "vault"
        instance_type = "t3.small"
        port_no = 8200
+    }
+    prometheus = {
+       name          = "prometheus"
+       instance_type = "t3.small"
+       port_no = 9090
     }
   }
 }
