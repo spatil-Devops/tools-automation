1849ce4b39b7bfec5053aceb6d220ddbd61814f7
Patil <shubham.patil5@dell.com>
2025-06-12 14:27:55 +0530

    tool_name

--- setup-tools.sh
+++ setup-tools.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i ${tool_name}-internal.sdevopsp25.site, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${tool_name} setup-tools.yml
+ansible-playbook -i ${tool_name}-internal.sdevopsp25.site, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e tool_name=${tool_name} setup-tools.yml
