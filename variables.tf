e1daa4fae08e99ebfb046a00370fb105dacc48ed
Patil <shubham.patil5@dell.com>
2025-06-14 13:33:03 +0530

    grafana

--- variables.tf
+++ variables.tf
@@ -15,5 +15,11 @@ variable "tools" {
           "ec2:DescribeAvailabilityZones"
         ]
     }
+    grafana = {
+       name          = "prometheus"
+       instance_type = "t3.small"
+       port_no = 3000
+       policy_actions = []
+    }
   }
 }
