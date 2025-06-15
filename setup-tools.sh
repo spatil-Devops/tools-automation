fc6e22e2cd78c1a654ceda2bc1f3a7af4425ef6e
Patil <shubham.patil5@dell.com>
2025-06-15 21:53:15 +0530

    install hvac

--- setup-tools.sh
+++ setup-tools.sh
@@ -1,1 +1,2 @@
+pip3.11 install hvac
 ansible-playbook -i ${tool_name}-internal.sdevopsp25.site, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e tool_name=${tool_name} setup-tools.yml -e vault_token=${vault_token}
