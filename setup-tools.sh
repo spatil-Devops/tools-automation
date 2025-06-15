328eea5af07fc129b7bc89d732b22fd9285e87cb
Patil <shubham.patil5@dell.com>
2025-06-15 20:35:10 +0530

    email configuration

--- setup-tools.sh
+++ setup-tools.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i ${tool_name}-internal.sdevopsp25.site, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e tool_name=${tool_name} setup-tools.yml
+ansible-playbook -i ${tool_name}-internal.sdevopsp25.site, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e tool_name=${tool_name} -e vault_token=${vault_token} setup-tools.yml
